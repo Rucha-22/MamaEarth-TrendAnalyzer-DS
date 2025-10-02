83edd0e80d3b862cabf045cd0f61a62ff1d13026
Joslyn J Abraham <89780037+StackedThoughts@users.noreply.github.com>
2025-10-02 18:59:01 +0530

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,10 +1,10 @@
+import os
+import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import classification_report, accuracy_score
-import joblib
-import os
 from preprocess import load_and_preprocess
 
 # Ensure src folder exists
@@ -35,7 +35,7 @@ y_pred = pipeline.predict(X_test)
 print("Accuracy:", accuracy_score(y_test, y_pred))
 print("\nClassification Report:\n", classification_report(y_test, y_pred))
 
-model = LogisticRegression()
-joblib.dump(model, "src/test_model.pkl")
-print("Model saved!")
-
+# Save the trained pipeline
+model_path = "src/sentiment_pipeline.pkl"
+joblib.dump(pipeline, model_path)
+print(f"✅ Model pipeline saved at: {model_path}")
